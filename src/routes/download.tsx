ab8598a6abcd9619ddef8130e0f59515e6b7a6a7
tkissdev <tkissdev@gmail.com>
2026-07-26 01:56:56 +0200

    Publier l'app Windows v1.0.8 (correctif installeur)

--- src/routes/download.tsx
+++ src/routes/download.tsx
@@ -17,8 +17,8 @@ export const Route = createFileRoute("/download")({
   component: DownloadPage,
 });
 
-const RELEASE_BASE = "https://github.com/tkissdev/readingtk/releases/download/windows-app-v1.0.7";
-const WINDOWS_VERSION = "1.0.7";
+const RELEASE_BASE = "https://github.com/tkissdev/readingtk/releases/download/windows-app-v1.0.8";
+const WINDOWS_VERSION = "1.0.8";
 
 const ChromeIcon = () => (
   <svg width="28" height="28" viewBox="0 0 24 24" fill="none">
